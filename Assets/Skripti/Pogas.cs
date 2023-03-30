0088477e802074b902c58462e95c8de1927c2841
Lauris <porinslauris@gmail.com>
2023-03-30 14:27:53 +0300

    Pievienot spēles aina

---
+++ Assets/Skripti/Pogas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pogas : MonoBehaviour {
+
+    public void uzGrutibasAinu() {
+        SceneManager.LoadScene ("grutibasAina", LoadSceneMode.Single);
+    }
+
+	public void uzSpelesAinu() {
+		SceneManager.LoadScene ("spelesAina", LoadSceneMode.Single);
+	}
+
+}
