0df2d5e5a7ca485a3b33d178d4d9d96bea71de10
Lauris <porinslauris@gmail.com>
2023-03-31 14:34:34 +0300

    Pirmās figūras kustības skripts

---
+++ Assets/Skripti/TetrisBloks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBloks : MonoBehaviour {
+
+	private float pagLaiks;
+	public float lidosanasLaiks = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			transform.position += new Vector3 (-1, 0, 0);
+	    }else if(Input.GetKeyDown(KeyCode.RightArrow)){
+			transform.position += new Vector3 (1, 0, 0);
+		}
+
+		if(Time.time - pagLaiks > (Input.GetKey(KeyCode.DownArrow) ? lidosanasLaiks/10 : lidosanasLaiks)){
+			transform.position += new Vector3(0, -1, 0);
+			pagLaiks = Time.time;
+		}
+	} 
+
+
+}
+
+		
