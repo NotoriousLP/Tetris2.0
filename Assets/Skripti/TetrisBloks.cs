86cc87d2b41639322aa57d714a6da96fb6ea6798
Lauris <porinslauris@gmail.com>
2023-04-04 08:43:47 +0300

    Pielikta vēlviena funkcija, bet nav vēl 100% ejoša

--- Assets/Skripti/TetrisBloks.cs
+++ Assets/Skripti/TetrisBloks.cs
@@ -6,6 +6,8 @@ public class TetrisBloks : MonoBehaviour {
 
 	private float pagLaiks;
 	public float lidosanasLaiks = 0.8f;
+	public static int augstums = 1080;
+	public static int platums = 1080;
 
 	// Use this for initialization
 	void Start () {
@@ -16,18 +18,41 @@ public class TetrisBloks : MonoBehaviour {
 	void Update () {
 		
 		if(Input.GetKeyDown(KeyCode.LeftArrow)){
-			transform.position += new Vector3 (-1, 0, 0);
+			transform.position += new Vector3 (-10, 0, 0);
+			if (!derigsGajiens ()) {
+				transform.position -= new Vector3 (-10, 0, 0);
+			}
+			
 	    }else if(Input.GetKeyDown(KeyCode.RightArrow)){
-			transform.position += new Vector3 (1, 0, 0);
+			transform.position += new Vector3 (10, 0, 0);
+			if (!derigsGajiens ()) {
+				transform.position -= new Vector3 (10, 0, 0);
+			}
 		}
 
 		if(Time.time - pagLaiks > (Input.GetKey(KeyCode.DownArrow) ? lidosanasLaiks/10 : lidosanasLaiks)){
-			transform.position += new Vector3(0, -1, 0);
+			transform.position += new Vector3(0, -10, 0);
+			if (!derigsGajiens ()) {
+				transform.position -= new Vector3 (0, -10, 0);
+			}
 			pagLaiks = Time.time;
 		}
 	} 
 
 
+	bool derigsGajiens() {
+		
+		foreach (Transform children in transform){
+			int roundedX = Mathf.RoundToInt(children.transform.position.x);
+			int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+			if(roundedX <0 || roundedX >=platums || roundedY < 0 || roundedY >= augstums){
+			   return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 		
